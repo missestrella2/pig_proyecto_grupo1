538059b1b0e13204fed7d825d5f2fa644bf7b241
missestrella2 <missestrella2@gmail.com>
2022-10-10 19:04:11 -0300

    creo las vistas y las urls de index clientes y productos

--- pig_django_app/pig_django_app/urls.py
+++ pig_django_app/pig_django_app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from prueba_clientes import views  #importo las vistas de "prueba clientes"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index) #entra a la funcion index de las views de prueba clientes 
 ]
--- pig_django_app/prueba_productos/views.py
+++ pig_django_app/prueba_productos/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse # segun clase 10
 
-def index(request):
-     #codigo
-     return HttpResponse("los productos")
-     
