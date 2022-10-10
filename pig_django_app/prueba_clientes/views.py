0c923e9f9863e1879087cb87b650d3ca1e29ba44
missestrella2 <missestrella2@gmail.com>
2022-10-10 18:29:18 -0300

    creacion de las vistas

--- pig_django_app/prueba_clientes/views.py
+++ pig_django_app/prueba_clientes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse # segun clase 10
+
+def index(request):
+     #codigo
+     return HttpResponse("los clientes")
