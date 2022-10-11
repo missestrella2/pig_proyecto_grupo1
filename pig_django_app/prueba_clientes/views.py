ae42751fbe31df45dfaf2019090fee7605da83d0
missestrella2 <missestrella2@gmail.com>
2022-10-10 22:06:19 -0300

    conexion de la plantilla con las urls y las views

--- pig_django_app/prueba_clientes/views.py
+++ pig_django_app/prueba_clientes/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render #CLASE 12 necesario para usar las plantillas
+from django.template import loader #CLASE 12 necesario para usar las plantillas
+from django.http import HttpResponse #CLASE 12 necesario para usar las plantillas
+from datetime import datetime 
 
 # Create your views here.
 
 from django.http import HttpResponse # segun clase 10
 
-def index(request):
+#def index(request):
      #codigo
-     return HttpResponse("soy el index")
+#     return HttpResponse("soy el index")
+
+def index(request):                                                #asi se cargan los templates
+    template = loader.get_template('prueba_clientes/index.html')   #crea objeto template que trae a index.html
+    context = {"hoy":datetime.now}                                 #creo context que es un diccionario
+    return HttpResponse(template.render(context,request))
+
 
 def clientes(request):
      #codigo
