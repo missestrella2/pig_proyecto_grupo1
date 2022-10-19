e03cc19796bc8c7ce043baaacf9d708b80010a3a
Jorgelina Fernandez <mickyomega7@gmail.com>
2022-10-19 12:04:41 -0300

    Se agrega el esqueleto de los metodos de pago

---
+++ pig_django_app/payments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class PaymentMethods(models.Model):
+    pass
+
+
+class Payments(models.Model):
+    """
+        Relacionar el producto, el cliente y metodo de pago
+    """
+    pass
