e8d29ca0ec664694a5015a40bb5f68619a58a0f2
missestrella2 <missestrella2@gmail.com>
2022-10-19 13:08:43 -0300

    todavia no anda la vista parametrizada

--- proyecto_version2/clientes/urls.py
+++ proyecto_version2/clientes/urls.py
@@ -5,6 +5,8 @@ urlpatterns=[
 
     path('clientes/',views.clientes, name="clientes"),
     path('clientes/clientes-listado-completo',views.clientes_listado_completo, name="clientes_listado_completo"),
-    # path('clientes/<int:year>',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta"),
-     path('clientes/2020',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta")
+    
+    #path('clientes/2020',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta")
+    #path('clientes/<int:year>',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta"),
+    
 ]
--- proyecto_version2/clientes/views.py
+++ proyecto_version2/clientes/views.py
@@ -21,6 +21,6 @@ def clientes_listado_completo(request):                                         
 
 def clientes_por_anio_de_alta(request):                                                #asi se cargan los templates
     template = loader.get_template('clientes/clientes-por-anio-de-alta.html')   #crea objeto template que trae a clientes.html
-    context = {}                                                               #creo context que es un diccionario
-                                                                               #aca dentro de context crear un diccionario que despues voy a  llamar desde la template usando las llaves
+    context = {{"elanio": "%r" %request.GET["elanio"] }}                                                               #creo context que es un diccionario
+                                                                                #aca dentro de context crear un diccionario que despues voy a  llamar desde la template usando las llaves
     return HttpResponse(template.render(context,request))
