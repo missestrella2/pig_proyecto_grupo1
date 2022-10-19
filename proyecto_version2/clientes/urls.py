323327690c4f3af4fd4c7d97ef816c5d2def4209
missestrella2 <missestrella2@gmail.com>
2022-10-19 12:32:41 -0300

    htmls views y urls de clientes. clientes listado completo. form para ingresar el anio (todavia no funciona). vista paratremizada del anio de alta d elos clientes

--- proyecto_version2/clientes/urls.py
+++ proyecto_version2/clientes/urls.py
@@ -4,4 +4,7 @@ from . import views #segun clase 10 significa: desde donde estoy importa las vis
 urlpatterns=[
 
     path('clientes/',views.clientes, name="clientes"),
+    path('clientes/clientes-listado-completo',views.clientes_listado_completo, name="clientes_listado_completo"),
+    # path('clientes/<int:year>',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta"),
+     path('clientes/2020',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta")
 ]
--- proyecto_version2/clientes/views.py
+++ proyecto_version2/clientes/views.py
@@ -8,9 +8,19 @@ from datetime import datetime
 from django.http import HttpResponse # segun clase 10
 
 def clientes(request):                                                #asi se cargan los templates
-    template = loader.get_template('clientes/clientes.html')   #crea objeto template que trae a index.html
+    template = loader.get_template('clientes/clientes.html')   #crea objeto template que trae a clientes.html
     context = {"hoy":datetime.now, "nombre": "Bienvenido/a %r" %request.GET["elnombre"] }                                 #creo context que es un diccionario
-    
+            #aca dentro de context crear un diccionario que despues voy a  llamar desde la template usando las llaves
     return HttpResponse(template.render(context,request))
 
+def clientes_listado_completo(request):                                                #asi se cargan los templates
+    template = loader.get_template('clientes/clientes-listado-completo.html')   #crea objeto template que trae a clientes.html
+    context = {}                                                               #creo context que es un diccionario
+                                                                               #aca dentro de context crear un diccionario que despues voy a  llamar desde la template usando las llaves
+    return HttpResponse(template.render(context,request))
 
+def clientes_por_anio_de_alta(request):                                                #asi se cargan los templates
+    template = loader.get_template('clientes/clientes-por-anio-de-alta.html')   #crea objeto template que trae a clientes.html
+    context = {}                                                               #creo context que es un diccionario
+                                                                               #aca dentro de context crear un diccionario que despues voy a  llamar desde la template usando las llaves
+    return HttpResponse(template.render(context,request))
