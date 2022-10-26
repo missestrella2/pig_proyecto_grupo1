daa18fa2eafd23aaedca28a080b62f068ed0b251
missestrella2 <missestrella2@gmail.com>
2022-10-26 20:12:19 -0300

    mi rama nueva. nuevo comienzo

--- pig_django_app/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path #segun clase 10
-from . import views #segun clase 10 significa: desde donde estoy importa las vistas
-
-urlpatterns=[
-
-    path('',views.index,name="login") #entra a la funcion index de las views de "login"
-
-]
