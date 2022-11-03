0183ab143d22563d956450e6a6085ac59defbc35
missestrella2 <missestrella2@gmail.com>
2022-11-03 11:56:06 -0300

    model de login. migraciones. configuracion de django extensions.se cargo un usuario por consola

--- proyecto_version2/login/models.py
+++ proyecto_version2/login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre= models.CharField(max_length=100, verbose_name='Nombre')
+    apellido= models.CharField(max_length=100, verbose_name='Apellido',null=True)
+    email= models.EmailField(max_length=150, verbose_name='Email')
+    dni= models.IntegerField(verbose_name='DNI')
+    password= models.CharField(max_length=50, verbose_name='Password')
+    
+
