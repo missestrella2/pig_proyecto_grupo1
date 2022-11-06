9886f40b33b993a71d7c98a1aa6f92aed83e3814
missestrella2 <111753332+missestrella2@users.noreply.github.com>
2022-11-06 18:36:28 -0300

    Update urls.py
    limpieza

--- proyecto_version2/clientes/urls.py
+++ proyecto_version2/clientes/urls.py
@@ -1,16 +1,1 @@
-from django.urls import path
-from . import views 
 
-urlpatterns=[
-
-    path('clientes/',views.clientes, name="clientes"),
-    path('clientes/clientes-listado-completo',views.clientes_listado_completo, name="clientes_listado_completo"),
-    path('clientes/altaclientes',views.altaclientes, name="altaclientes"),
-    path('clientes/bajaclientes',views.bajaclientes, name="bajaclientes"),
-
-
-    #path('clientes/2020',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta")
-
-    path('clientes/<int:anio>',views.clientes_por_anio_de_alta, name="clientes_por_anio_de_alta"),
-    
-]
