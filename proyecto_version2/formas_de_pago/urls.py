7c306cd9dda62d6d2d287f08d080853ed5cd1672
missestrella2 <111753332+missestrella2@users.noreply.github.com>
2022-11-06 18:34:15 -0300

    Update urls.py
    limpieza

--- proyecto_version2/formas_de_pago/urls.py
+++ proyecto_version2/formas_de_pago/urls.py
@@ -1,9 +1,1 @@
-from django.urls import path #segun clase 10
-from . import views #segun clase 10 significa: desde donde estoy importa las vistas
 
-urlpatterns=[
-
-    path('formas-de-pago/',views.formas_de_pago, name="formas_de_pago"),
-    path('formas-de-pago/alta-medio-de-pago',views.alta_medio_de_pago, name="alta_medio_de_pago"),
-    path('formas-de-pago/baja-medio-de-pago',views.baja_medio_de_pago, name="baja_medio_de_pago"),
-]
