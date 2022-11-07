b01919501f7d01981181a52eb772041e9eb49e81
missestrella2 <missestrella2@gmail.com>
2022-11-06 23:19:50 -0300

    en la app de usuarios ya tenia el models.py. hice la relacion de uno a muchos con foreignkey. hice makemigration y migrate

--- proyecto_version2/usuarios/models.py
+++ proyecto_version2/usuarios/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+class Cargo(models.Model):
+    nombre= models.CharField(max_length=100,default=" ", verbose_name='Nombre')
+    baja=models.BooleanField(default=False)
+
 class Usuario(models.Model):
-    nombre= models.CharField(max_length=100, verbose_name='Nombre')
-    apellido= models.CharField(max_length=100, verbose_name='Apellido')
+    nombre= models.CharField(max_length=100, default=" ", verbose_name='Nombre')
+    apellido= models.CharField(max_length=100, null=True, verbose_name='Apellido')
     email= models.EmailField(max_length=150, verbose_name='Email')
     password= models.CharField(max_length=50, verbose_name='Password')
     fechadealta=models.DateField(verbose_name='Fecha de Alta')
-
-class Cargo(models.Model):
-    cargo= models.CharField(max_length=100, verbose_name='Cargo')
+    cargo = models.ForeignKey(Cargo,default="",on_delete=models.CASCADE)
