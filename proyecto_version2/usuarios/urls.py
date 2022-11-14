8742b03775614c937e28f763f54b6ed55f3c9c94
missestrella2 <missestrella2@gmail.com>
2022-11-14 20:23:46 -0300

    probando las vistas en usuarios

--- proyecto_version2/usuarios/urls.py
+++ proyecto_version2/usuarios/urls.py
@@ -1,20 +1,25 @@
-from django.urls import path, re_path #segun clase 10
-from . import views #segun clase 10 significa: desde donde estoy importa las vistas
-from django import forms
+from django.urls import path, re_path
+from . import views
 
 urlpatterns=[
 
-    path('usuarios/usuariosform',views.usuariosform,name="usuariosform"),
-    path('usuarios/altausuarioform',views.altausuarioform,name="altausuarioform"),
-    path('usuarios/bajausuarioform',views.bajausuarioform,name="bajausuarioform"),
+    #path('usuarios/usuariosform',views.usuariosform,name="usuariosform"),
+    # path('usuarios/altausuarioform',views.altausuarioform,name="altausuarioform"),
+    # path('usuarios/bajausuarioform',views.bajausuarioform,name="bajausuarioform"),
     
-    path('usuarios/resultadoalta/',views.resultadoalta,name="resultadoalta"),
-    path('usuarios/resultadobaja/',views.resultadobaja,name="resultadobaja"),
-    path('usuarios/resultadofiltro/',views.resultadofiltro,name="resultadofiltro"),
+    # path('usuarios/resultadoalta/',views.resultadoalta,name="resultadoalta"),
+    # path('usuarios/resultadobaja/',views.resultadobaja,name="resultadobaja"),
+    # path('usuarios/resultadofiltro/',views.resultadofiltro,name="resultadofiltro"),
 
 #ejemplo que fnciona de formulario
-    path('usuarios/busqueda_productos/',views.busqueda_productos,name="busqueda_productos"),
-    path('usuarios/buscar/',views.buscar,name="buscar"),
- 
+    #path('usuarios/busqueda_productos/',views.busqueda_productos,name="busqueda_productos"),
+    #path('usuarios/buscar/',views.buscar,name="buscar"),
+    
+    
+    path('usuarios/listadeusuarios/',views.ListaDeUsuarios.as_view(),name='listadeusuarios'),
+    path('usuarios/usuarioeditar/<int:id_usuario>',views.usuarioeditar,name='usuarioeditar'),
+    path('usuarios/usuarioeliminar/<int:id_usuario>',views.usuarioeliminar,name='usuarioeliminar'),
+
+
 ]
 
--- proyecto_version2/usuarios/views.py
+++ proyecto_version2/usuarios/views.py
@@ -1,175 +1,205 @@
-
 from datetime import datetime
 from multiprocessing import context
 from django.http import HttpResponse, JsonResponse
-
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
 from django.template import loader
+from usuarios.forms import UsuariosForm
 from django.contrib import messages
 
-from django.views import View
-from django.views.generic import ListView
-from .forms import UsuariosForm  # segun la clase de forms
-from .forms import AltaUsuarioForm
-from .forms import BajaUsuarioForm
-from .models import Articulos, Usuario
-#from .models import Cargo
-from django import forms
+from usuarios.models import Usuario
 from django.views import View
 from django.views.generic import ListView
 
-####importacion de los forms de ejemplo####
-from usuarios.forms import FormularioFiltrado
+class ListaDeUsuarios(ListView):
+    model = Usuario 
+    context_object_name = 'usuarios'
+    template_name = 'usuarios/listadeusuarios.html'
+    ordering =['id']
+
+def usuarioeditar(request, id_usuario):
+    try:
+        usuario = Usuario.objects.get(id=id_usuario)
+    except Usuario.DoesNotExist:
+        return render(request, '404.html')
+    
+    if request.method == "POST":
+        formulario = UsuariosForm(request.POST, instance=usuario)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('listadeusuarios')
+    else:
+        formulario = UsuariosForm(instance=usuario)
+
+    return render(request, 'usuarios/usuarioeditar.html', {'formulario': formulario, 'id_usuario': id_usuario})
+
+
+def usuarioeliminar(request, id_usuario):
+    try:
+        usuario = Usuario.objects.get(id=id_usuario)
+    except Usuario.DoesNotExist:
+        return render(request, 'usuarios/404.html')
+    
+    try:
+        usuario.delete()
+    except ValueError as ve:
+        messages.error(request=request, message="no se puede borrar")
+    return redirect('listadeusuarios')
+
 
 #def index(request):
      #codigo
 #     return HttpResponse("soy el index")
 
-def usuariosform(request):
-    if request.method == 'POST':
-        usuariosform = UsuariosForm(request.POST)
-    else:
-        usuariosform = UsuariosForm()
-    return render(request, 'usuarios/usuariosform.html', {'usuariosform': usuariosform})
+# def usuariosform(request):
+#      if request.method == 'POST':
+#          usuariosform = UsuariosForm(request.POST)
+#      else:
+#          usuariosform = UsuariosForm()
+#      return render(request, 'usuarios/usuariosform.html', {'usuariosform': usuariosform})
 
-def resultadofiltro(request):
-    # ejemplo basado en pildoras informaticas
-    if request.method == 'POST':
-         usuariosform = UsuariosForm(request.POST)
-         if usuariosform.is_valid():
-            nombre = request.POST["nombre"]
-            apellido = request.POST["apellido"]
-            email = request.POST["email"]
-            usuarios=Usuario.objects.filter(nombre__icontains=nombre,apellido__icontains=apellido,email__icontains=email) #icontains seria LIKE en sql
-    return render(request, "usuarios/resultadofiltro.html",{"usuarios":usuarios,"query":nombre,"usuariosform":usuariosform})
+
+
+# def resultadofiltro(request):
+#     # ejemplo basado en pildoras informaticas
+#     if request.method == 'POST':
+#          usuariosform = UsuariosForm(request.POST)
+#          if usuariosform.is_valid():
+#             nombre = request.POST["nombre"]
+#             apellido = request.POST["apellido"]
+#             email = request.POST["email"]
+#             usuarios=Usuario.objects.filter(nombre__icontains=nombre,apellido__icontains=apellido,email__icontains=email) #icontains seria LIKE en sql
+#     return render(request, "usuarios/resultadofiltro.html",{"usuarios":usuarios,"query":nombre,"usuariosform":usuariosform})
     
-    # ejemplo visto en clase aun no logro hacerlo funcionar
-    #               usuariosform = Usuario.objects.all().order_by('id') 
-    #       return render(request, "usuarios/resultadofiltro.html",{"usuariosform":usuariosform})
+#     # ejemplo visto en clase aun no logro hacerlo funcionar
+#     #               usuariosform = Usuario.objects.all().order_by('id') 
+#     #       return render(request, "usuarios/resultadofiltro.html",{"usuariosform":usuariosform})
 
-def altausuarioform(request): 
-    if request.method == 'POST':
-        altausuarioform = AltaUsuarioForm(request.POST)
-        if altausuarioform.is_valid():
-            nombre = altausuarioform.cleaned_data['nombre']
-            apellido = altausuarioform.cleaned_data['apellido']
-            email = altausuarioform.cleaned_data['email']
-            password = altausuarioform.cleaned_data['password']
-    #       cargo = altausuarioform.cleaned_data['cargo']
-            nuevo_usuario = Usuario(nombre=nombre,apellido=apellido,email=email,password=password)
-            try:
-                nuevo_usuario.save()
-            except ValueError as ve:
-                altausuarioform.add_error('apellido', str(ve))
-            else:
-                return redirect('altausuarioform')
-    else:
-        altausuarioform = AltaUsuarioForm()
-        return render(request, 'usuarios/altausuarioform.html', {'altausuarioform': altausuarioform})
-
-
-def bajausuarioform(request): 
-    if request.method == 'POST':
-        bajausuarioform = BajaUsuarioForm(request.POST)
-        if bajausuarioform.is_valid():
-            email = bajausuarioform.cleaned_data['email']
-            password = bajausuarioform.cleaned_data['password']
-    #       cargo = altausuarioform.cleaned_data['cargo']
-            usuario_a_borrar= Usuario.objects.get(email=email,password=password)
-            try:
-                usuario_a_borrar.delete()
-            except ValueError as ve:
-                bajausuarioform.add_error('email', str(ve))
-            else:
-                return redirect('bajausuarioform')
-    else:
-        bajausuarioform = BajaUsuarioForm()
-        return render(request, 'usuarios/bajausuarioform.html', {'bajausuarioform': bajausuarioform})
+# def altausuarioform(request): 
+#     if request.method == 'POST':
+#         altausuarioform = AltaUsuarioForm(request.POST)
+#         if altausuarioform.is_valid():
+#             nombre = altausuarioform.cleaned_data['nombre']
+#             apellido = altausuarioform.cleaned_data['apellido']
+#             email = altausuarioform.cleaned_data['email']
+#             password = altausuarioform.cleaned_data['password']
+#     #       cargo = altausuarioform.cleaned_data['cargo']
+#             nuevo_usuario = Usuario(nombre=nombre,apellido=apellido,email=email,password=password)
+#             try:
+#                 nuevo_usuario.save()
+#             except ValueError as ve:
+#                 altausuarioform.add_error('apellido', str(ve))
+#             else:
+#                 return redirect('altausuarioform')
+#     else:
+#         altausuarioform = AltaUsuarioForm()
+#         return render(request, 'usuarios/altausuarioform.html', {'altausuarioform': altausuarioform})
 
 
+# def bajausuarioform(request): 
+#     if request.method == 'POST':
+#         bajausuarioform = BajaUsuarioForm(request.POST)
+#         if bajausuarioform.is_valid():
+#             email = bajausuarioform.cleaned_data['email']
+#             password = bajausuarioform.cleaned_data['password']
+#     #       cargo = altausuarioform.cleaned_data['cargo']
+#             try:
+#                 usuario_a_borrar= Usuario.objects.get(email=email,password=password)
+#             except Usuario.DoesNotExist:
+#                 return render(request, "usuarios/404.html")
 
-
-
-#Ejemplo visto en clase con boton y parametrizado
-# def bajausuarioform(request,email_usuario): 
-# #id_usuario):
-#      try:
-#          usuario=Usuario.objects.get(email=email_usuario)
-#      except Usuario.DoesNotExist:
-#          return render(request, "usuarios/404.html")
-
-#      try:
-#          usuario.delete()
-#      except ValueError as ve:
-#          messages.error(request=request,
-#                         message="No se puede borrar al Gerente")
-#      return redirect('usuariosform')    
+#             try:
+#                 usuario_a_borrar.delete()
+#             except ValueError as ve:
+#                 bajausuarioform.add_error('email', str(ve))
+#             else:
+#                 return redirect('bajausuarioform')
+#     else:
+#         bajausuarioform = BajaUsuarioForm()
+#         return render(request, 'usuarios/bajausuarioform.html', {'bajausuarioform': bajausuarioform})
 
 
 
 
-##########EN EL FUTURO LOS FORMULARIOS DEBEN REDIRIGIR ACA ###########
+
+# #Ejemplo visto en clase con boton y parametrizado
+# # def bajausuarioform(request,email_usuario): 
+# # #id_usuario):
+# #      try:
+# #          usuario=Usuario.objects.get(email=email_usuario)
+# #      except Usuario.DoesNotExist:
+# #          return render(request, "usuarios/404.html")
+
+# #      try:
+# #          usuario.delete()
+# #      except ValueError as ve:
+# #          messages.error(request=request,
+# #                         message="No se puede borrar al Gerente")
+# #      return redirect('usuariosform')    
 
 
-def resultadoalta(request):
-        mensaje = "Se dio de alta a %r" %request.POST["nombre"]
-        return HttpResponse(mensaje)
-
-def resultadobaja(request):
-        mensaje="Se dio de baja a %r" %request.POST["nombre"]
-        return HttpResponse(mensaje)
 
 
+# # ##########EN EL FUTURO LOS FORMULARIOS DEBEN REDIRIGIR ACA ###########
 
 
-#######Ejemplo formulario 1
+# def resultadoalta(request):
+#         mensaje = "Se dio de alta a %r" %request.POST["nombre"]
+#         return HttpResponse(mensaje)
 
-# def busqueda_productos(request):
-#         return render(request, 'usuarios/busqueda_productos.html')
-
-# def buscar(request):
-#         mensaje="Se dio de alta a %r" %request.GET["prd"]
+# def resultadobaja(request):
+#         mensaje="Se dio de baja a %r" %request.POST["nombre"]
 #         return HttpResponse(mensaje)
 
 
-#######Ejemplo formulario 2 (con base de datos)###########
-
-#def busqueda_productos(request):
-#         return render(request, 'usuarios/busqueda_productos.html')
-
-#def buscar(request):
-#     if request.GET["prd"]:
-#         #mensaje="Se dio de alta a %r" %request.GET["prd"]
-#         producto = request.GET["prd"]
-#         articulos=Articulos.objects.filter(nombre__icontains=producto) #icontains seria LIKE en sql
-#         return render(request, "usuarios/resultados_busqueda.html",{"articulos":articulos,"query":producto})
-#     else:
-#         mensaje="no has introducido nada"
-#     return HttpResponse(mensaje)
-
-#######Ejemplo formulario 3 (con base de datos)###########
-
-def busqueda_productos(request):
-#         if request.method == 'POST':
-#             miFormulario = FormularioFiltrado(request.GET)
-#         else:
-#             miFormulario = FormularioFiltrado()
-         return render(request, 'usuarios/busqueda_productos.html',{'miFormulario':miFormulario})
 
 
-def buscar(request):
-     if request.method=="GET":
-#         miFormulario=FormularioFiltrado(request.GET)
+# # #######Ejemplo formulario 1
 
-#         if miFormulario.is_valid():
-#           producto = request.GET["nombre"]
-#           articulos=Articulos.objects.filter(nombre__icontains=producto) #icontains seria LIKE en sql
-           return render(request, "usuarios/resultados_busqueda.html",{"articulos":articulos,"query":producto,"miFormulario":miFormulario})
+# # # def busqueda_productos(request):
+# # #         return render(request, 'usuarios/busqueda_productos.html')
 
-     else:
-         miFormulario=FormularioFiltrado()    
+# # # def buscar(request):
+# # #         mensaje="Se dio de alta a %r" %request.GET["prd"]
+# # #         return HttpResponse(mensaje)
+
+
+# # #######Ejemplo formulario 2 (con base de datos)###########
+
+# # #def busqueda_productos(request):
+# # #         return render(request, 'usuarios/busqueda_productos.html')
+
+# # #def buscar(request):
+# # #     if request.GET["prd"]:
+# # #         #mensaje="Se dio de alta a %r" %request.GET["prd"]
+# # #         producto = request.GET["prd"]
+# # #         articulos=Articulos.objects.filter(nombre__icontains=producto) #icontains seria LIKE en sql
+# # #         return render(request, "usuarios/resultados_busqueda.html",{"articulos":articulos,"query":producto})
+# # #     else:
+# # #         mensaje="no has introducido nada"
+# # #     return HttpResponse(mensaje)
+
+# # #######Ejemplo formulario 3 (con base de datos)###########
+
+# # def busqueda_productos(request):
+# # #         if request.method == 'POST':
+# # #             miFormulario = FormularioFiltrado(request.GET)
+# # #         else:
+# # #             miFormulario = FormularioFiltrado()
+# #          return render(request, 'usuarios/busqueda_productos.html',{'miFormulario':miFormulario})
+
+
+# # def buscar(request):
+# #      if request.method=="GET":
+# # #         miFormulario=FormularioFiltrado(request.GET)
+
+# # #         if miFormulario.is_valid():
+# # #           producto = request.GET["nombre"]
+# # #           articulos=Articulos.objects.filter(nombre__icontains=producto) #icontains seria LIKE en sql
+# #            return render(request, "usuarios/resultados_busqueda.html",{"articulos":articulos,"query":producto,"miFormulario":miFormulario})
+
+# #      else:
+# #          miFormulario=FormularioFiltrado()    
 
 
 
