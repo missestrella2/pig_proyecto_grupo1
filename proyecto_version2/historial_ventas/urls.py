8506675ccc315d4ed05d56dc26f705d199b240e7
missestrella2 <missestrella2@gmail.com>
2022-12-05 16:35:44 -0300

    se agrego login_required

--- proyecto_version2/historial_ventas/urls.py
+++ proyecto_version2/historial_ventas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path #segun clase 10
 from . import views #segun clase 10 significa: desde donde estoy importa las vistas
+from django.contrib.auth.decorators import login_required
 
 urlpatterns=[
 
-    path('historial-ventas/',views.historial_ventas,name="historial_ventas"),
-    path('historial-ventas/histventform',views.histventform,name="histventform"),
+    path('historial-ventas/',login_required(views.historial_ventas),name="historial_ventas"),
+   
 ]
